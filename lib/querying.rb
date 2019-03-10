dd683cd2fd65125bae825c7da2516c92c86e2eb3
Amil Masic <amil.masic@gmail.com>
2019-03-10 23:11:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species COUNT(species) GROUP BY species"
+  "SELECT species COUNT(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
