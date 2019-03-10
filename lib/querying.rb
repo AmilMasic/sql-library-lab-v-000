df7eb809eda536846ac0ad2f35317207557fa18e
Amil Masic <amil.masic@gmail.com>
2019-03-10 23:31:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,6 +26,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title from characters
   INNER JOIN series ON series.id = characters.series_id
+  GROUP BY series.title
   "
 end
 
